51dc71c71641fe22a02b275d9aa0a86601c61d7a
Chris Didcote <chris.didcote@computerminds.co.uk>
2015-05-19 18:03:06 +0100

    some more work on the new responsive theme

--- themes/custom/mha_res/js/global.js
+++ themes/custom/mha_res/js/global.js
@@ -2,7 +2,6 @@
   Drupal.behaviors.mha_res = {
     attach: function (context, settings) {
       $("img").addClass( "img-responsive" );
-      $("#block-system-main-menu li").addClass( "navbar-left" );
     }
   };
 
